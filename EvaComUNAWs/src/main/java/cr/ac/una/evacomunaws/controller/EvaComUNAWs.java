a27755a9b0362de47a58b1119bed2ab28eb7b25e
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-08-23 13:25:30 -0600

    WDSL
    Para probar lo del WDSL es mejor descargar el soap UI porque no deja levantar el tester

---
+++ EvaComUNAWs/src/main/java/cr/ac/una/evacomunaws/controller/EvaComUNAWs.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package cr.ac.una.evacomunaws.controller;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+
+
+/**
+ *
+ * @author kevin
+ */
+@WebService(serviceName = "EvaComUNAWs")
+public class EvaComUNAWs {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
