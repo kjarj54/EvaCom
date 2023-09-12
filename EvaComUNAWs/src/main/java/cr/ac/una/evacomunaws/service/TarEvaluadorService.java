c01ccb0ddd16921edc015aa29a158c248e1e2e85
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-09-12 11:42:49 -0600

    Services(en proceso)

--- EvaComUNAWs/src/main/java/cr/ac/una/evacomunaws/service/TarEvaluadorService.java
+++ EvaComUNAWs/src/main/java/cr/ac/una/evacomunaws/service/TarEvaluadorService.java
@@ -4,10 +4,21 @@
  */
 package cr.ac.una.evacomunaws.service;
 
+import cr.ac.una.evacomunaws.model.TarEvaluador;
+import cr.ac.una.evacomunaws.model.TarEvaluadorDto;
+import cr.ac.una.evacomunaws.util.CodigoRespuesta;
+import cr.ac.una.evacomunaws.util.Respuesta;
 import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.NonUniqueResultException;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -19,5 +30,87 @@ import java.util.logging.Logger;
 public class TarEvaluadorService {
    private static final Logger LOG = Logger.getLogger(TarEvaluadorService.class.getName());
     @PersistenceContext(unitName = "EvaComUNAPU")
-    private EntityManager em; 
+    private EntityManager em;
+    
+    public Respuesta guardarEvaluador(TarEvaluadorDto tarEvaluadorDto) {
+        try {
+            TarEvaluador evaluador;
+            if (tarEvaluadorDto.getEvaluId()!= null && tarEvaluadorDto.getEvaluId()> 0) {
+                evaluador = em.find(TarEvaluador.class, tarEvaluadorDto.getEvaluId());
+                if (evaluador == null) {
+                    return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No se encrontró el usuario a modificar.", "guardarCaracteristica NoResultException");
+                }
+                evaluador.actualizar(tarEvaluadorDto);
+                evaluador = em.merge(evaluador);
+            } else {
+                evaluador = new TarEvaluador(tarEvaluadorDto);
+                em.persist(evaluador);
+            }
+            em.flush();
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "", "TarUsuario", new TarEvaluadorDto(evaluador));
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al guardar el usuario.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al guardar el usuario.", "guardarUsuario " + ex.getMessage());
+        }
+    }
+    
+    public Respuesta eliminarEvaluador(Long id) {
+        try {
+            TarEvaluador evaluador;
+            if (id != null && id > 0) {
+                evaluador = em.find(TarEvaluador.class, id);
+                if (evaluador == null) {
+                    return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No se encrontró el usuario a eliminar.", "eliminarUsuario NoResultException");
+                }
+                em.remove(evaluador);
+            } else {
+                return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "Debe cargar el usuario a eliminar.", "eliminarUsuario NoResultException");
+            }
+            em.flush();
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "");
+        } catch (Exception ex) {
+            if (ex.getCause() != null && ex.getCause().getCause().getClass() == SQLIntegrityConstraintViolationException.class) {
+                return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "No se puede eliminar el usuario porque tiene relaciones con otros registros.", "eliminarUsuario " + ex.getMessage());
+            }
+            LOG.log(Level.SEVERE, "Ocurrio un error al guardar la caracteristica.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al eliminar el usuario.", "eliminarUsuario " + ex.getMessage());
+        }
+    }
+    
+    public Respuesta getEvaluadores() {
+        try {
+            Query qryEvaluador = em.createNamedQuery("", TarEvaluador.class);
+            List<TarEvaluador> evaluadores = qryEvaluador.getResultList();
+            List<TarEvaluadorDto> evaluadorDto = new ArrayList<>();
+            for (TarEvaluador evaluador : evaluadores) {
+                evaluadorDto.add(new TarEvaluadorDto(evaluador));
+            }
+
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "", "Usuario", evaluadorDto);
+
+        } catch (NoResultException ex) {
+            return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No existen usuarios con los criterios ingresados.", "getUsuarios NoResultException");
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al consultar usuarios.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar usuarios.", "getUsuarios " + ex.getMessage());
+        }
+    }
+    
+    public Respuesta getEvaluador(Long id) {
+        try {
+            Query qryCaracteristica = em.createNamedQuery("TarUsuario.findBy", TarEvaluador.class);
+            qryCaracteristica.setParameter("id", id);
+
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "", "Empleado", new TarEvaluadorDto((TarEvaluador) qryCaracteristica.getSingleResult()));
+
+        } catch (NoResultException ex) {
+            return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No existe un usuario con el código ingresado.", "getUsuario NoResultException");
+        } catch (NonUniqueResultException ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al consultar el usuario.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar el usuario.", "getUsuario NonUniqueResultException");
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al consultar el usuario.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar el usuario.", "getUsuario " + ex.getMessage());
+        }
+    }
 }
