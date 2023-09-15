ce6255739b9c5306b2c2b1e5c6e2f62bd53068ac
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-09-15 15:01:25 -0600

    Conexion web service-cliente
    Tener cuidado, si da error en la conexion es solo hacer un refresh o volver a cargar el wsdl en el pom del proyecto nada mas tener cuidado y hacer refresh al proyecto y los clean and build cuando se haga un cambio en un metodo del web service

--- EvaComUNA/src/main/java/module-info.java
+++ EvaComUNA/src/main/java/module-info.java
@@ -4,10 +4,16 @@ module cr.ac.una.evacomuna {
     requires java.base;
     requires java.logging;
     requires com.jfoenix;
+    requires jakarta.xml.bind;
+    requires jakarta.xml.soap;
+    requires jakarta.xml.ws;
+    requires java.xml.ws;
+   
     
     opens cr.ac.una.evacomuna to javafx.fxml,java.base,com.jfoenix;
     opens cr.ac.una.evacomuna.controller to javafx.fxml;
     opens cr.ac.una.evacomuna.util to javafx.fxml;
+    opens cr.ac.una.evacomunaws.controller;
     
     exports cr.ac.una.evacomuna;
     exports cr.ac.una.evacomuna.controller;
