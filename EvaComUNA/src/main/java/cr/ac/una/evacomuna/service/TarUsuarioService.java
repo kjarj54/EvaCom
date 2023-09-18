9155e9ba1188608e279a21ae7aee9e2379829054
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-09-18 14:17:00 -0600

    Service usuario cliente

--- EvaComUNA/src/main/java/cr/ac/una/evacomuna/service/TarUsuarioService.java
+++ EvaComUNA/src/main/java/cr/ac/una/evacomuna/service/TarUsuarioService.java
@@ -4,10 +4,49 @@
  */
 package cr.ac.una.evacomuna.service;
 
+import cr.ac.una.evacomuna.util.Respuesta;
+import cr.ac.una.evacomunaws.controller.EvaComUNAWs;
+import cr.ac.una.evacomunaws.controller.EvaComUNAWs_Service;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author kevin
  */
 public class TarUsuarioService {
+    EvaComUNAWs evaComUNAWs;
+
+    public Respuesta guardarUsuario(cr.ac.una.evacomunaws.controller.TarUsuarioDto usuario) {
+        try {
+            EvaComUNAWs_Service canchaUNAWs_service = new EvaComUNAWs_Service();
+            evaComUNAWs = canchaUNAWs_service.getEvaComUNAWsPort();
+            evaComUNAWs.guardarUsuario(usuario);
+           if (false) {
+                return new Respuesta(false, "", "");
+            }
+            cr.ac.una.evacomunaws.controller.TarUsuarioDto tarUsuarioDto = (cr.ac.una.evacomunaws.controller.TarUsuarioDto) evaComUNAWs.guardarUsuario(usuario);
+            return new Respuesta(true, "", "", "TarUsuario", tarUsuarioDto);
+        } catch (Exception ex) {
+            Logger.getLogger(TarUsuarioService.class.getName()).log(Level.SEVERE, "Error guardando el usuario.", ex);
+            return new Respuesta(false, "Error guardando el usuario.", "guardarUsuario " + ex.getMessage());
+        }
+    }
     
+     public Respuesta getUsuario(Long id) {
+        try {
+            //TODO
+            EvaComUNAWs_Service canchaUNAWs_service = new EvaComUNAWs_Service();
+            evaComUNAWs = canchaUNAWs_service.getEvaComUNAWsPort();
+            evaComUNAWs.getUsuarioClass(id);
+            if (false) {
+                return new Respuesta(false, "", "");
+            }
+            cr.ac.una.evacomunaws.controller.TarUsuarioDto tarUsuarioDto = (cr.ac.una.evacomunaws.controller.TarUsuarioDto) evaComUNAWs.getUsuarioClass(id);
+            return new Respuesta(true, "", "", "TarUsuario", tarUsuarioDto);
+        } catch (Exception ex) {
+            Logger.getLogger(TarUsuarioService.class.getName()).log(Level.SEVERE, "Error obteniendo el usuario [" + id + "]", ex);
+            return new Respuesta(false, "Error obteniendo el usuario.", "getUsuario" + ex.getMessage());
+        }
+    }
 }
